b66b27e636041b6db68c88a9c28b62b875cf5d27
Pushkar2706 <pushkar9336@gmail.com>
2025-03-28 16:44:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,10 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
 // Fun Fact button functionality
 function showFact() {
     const facts = [
+        "Wearing a tie can reduce blood flow to the brain by 7.5 per cent.",
         "Did you know? The first website was published in 1991!",
+        "The fear of long words is called Hippopotomonstrosesquippedaliophobia.",
         "Water can boil and freeze at the same time under certain conditions.",
         "Bananas are berries, but strawberries are not!",
-        "Octopuses have three hearts and blue blood!"
+        "Octopuses have three hearts and blue blood!",
+        "Giraffes are 30 times more likely to get hit by lightning than people.",
+        "The largest piece of fossilised dinosaur poo discovered is over 30cm long and over two litres in volume.",
+        "The Universe's average colour is called 'Cosmic latte",
+        "Animals can experience time differently from humans.",
+        "Water might not be wet.",
+        "All the world’s bacteria stacked on top of each other would stretch for 10 billion light-years.",
+        "Octopuses don’t actually have tentacles."
+        
     ];
     document.getElementById("fact").innerText = facts[Math.floor(Math.random() * facts.length)];
 }
